e2123fefa2599510c34598d8900c1e1eaff86873
rakshithav2004 <rakshithabaiv@gmail.com>
2026-08-22 08:05:25 +0530

    add eureka registry

--- order-service/src/main/java/com/ecommerce/order/client/ProductClient.java
+++ order-service/src/main/java/com/ecommerce/order/client/ProductClient.java
@@ -7,10 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(
-        name = "product-service",
-        url = "${product-service.url}"
-)
+@FeignClient(name = "product-service")
 public interface ProductClient {
 
     @GetMapping("/api/v1/products/{id}")
