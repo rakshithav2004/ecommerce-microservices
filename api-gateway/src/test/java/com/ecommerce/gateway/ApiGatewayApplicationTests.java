bb3a3030f7cd0a0785b87db410c10052a16f518f
rakshithav2004 <rakshithabaiv@gmail.com>
2026-08-30 14:53:57 +0530

    Fix CI MongoDB test configuration

--- api-gateway/src/test/java/com/ecommerce/gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/ecommerce/gateway/ApiGatewayApplicationTests.java
@@ -3,7 +3,7 @@ package com.ecommerce.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "jwt.secret=test-secret-key-for-testing-only")
 class ApiGatewayApplicationTests {
 
   @Test
